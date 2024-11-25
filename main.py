5e9e7c3fc5c05d3f9eea0e9862c5f5e23376aeb3
pigmaster96 <malcolmtan2095@gmail.com>
2024-11-25 22:53:58 +0000

    main and other stuff

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 def main():
+    pygame.init()
+    window=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while True:
+        clock=pygame.time.Clock()
+        dt=0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(window, (0,0,0))
+        pygame.display.flip()
+        dt=clock.tick(60)/1000
     print("Starting asteroids!")
-    
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 
 
 
